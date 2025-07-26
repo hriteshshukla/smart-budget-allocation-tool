20fb743c49347db98566b5b6c0d4b6988955ca5f
Hritesh Shukla <hriteshshukla@outlook.com>
2025-07-26 02:49:19 +0100

    smart_budget_tool.py

--- smart_budget_tool.py
+++ smart_budget_tool.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1s7iuTL-ndy1dhvDL91hcIcEoIjNlTMG0
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
